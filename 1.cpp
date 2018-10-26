69128413b989fabf395fe22b5a5d8db99ba3705d
ArtyomKey <usertest@test.com>
2018-10-26 05:22:53 +0300

    second commit

--- 1.cpp
+++ 1.cpp
@@ -3,27 +3,30 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+
 using namespace std;
-char * prime_test(const int a){
+
+const char * prime_test(const int a){
     const int m = sqrt(a);
     if(a==1){
         return "unite";
-    }else {
-        if((a==2)){
-            return "prime";
-        }else {
-            if(a%2==0){
-                return "composite";
-            }else{
-                for(int i = 3; i <= m; i+=2){
-                    if(a%i==0){
-                        return "composite";
-                    };
-                };
-            };
-            };
-    };
- return "prime";
+    }
+    
+    if (a<=3){
+        return "prime";
+    }
+    
+    if (a%2==0){
+        return "composite";
+    }
+     
+    for(int i = 3; i <= m; i+=2){
+        if (a%i==0) {
+            return "composite";
+        }
+    }
+    
+    return "prime";
 }
 
 int main()
@@ -32,4 +35,3 @@ int main()
     cin >> a;
     cout << prime_test(a);
 }
-
